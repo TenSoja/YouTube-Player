dacf1e0a0b44dcf714dc1bce9d2c96f028af73b2
michel <tensoja@gmail.com>
2015-04-01 18:03:47 -0300

    Add buttons and onClick methods

--- src/org/example/youtube_player/MainActivity.java
+++ src/org/example/youtube_player/MainActivity.java
@@ -1,16 +1,44 @@
 package org.example.youtube_player;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
+	
+	Button btnPlay;
+	Button btnStandAlone;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnPlay = (Button) findViewById(R.id.btnPlay);
+		btnPlay.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, YoutubeActivity.class);
+				startActivity(intent);
+				
+			}
+		});
+		
+		btnStandAlone = (Button) findViewById(R.id.btnSubMenu);
+		
+		btnStandAlone.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, StandAloneActivity.class);
+				startActivity(intent);
+				
+			}
+		});
 	}
 
 	@Override
